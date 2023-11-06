81b35e1dc8feddc5c6bc75f34b070f83805367f6
Afif Pristantio-A11.2020.12993 <afifpristantio@gmail.com>
2023-11-07 06:04:50 +0700

    revisi 8

--- index.php
+++ index.php
@@ -120,6 +120,9 @@ session_start();
         ?>
     </main>
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
     <script>
     $(document).ready(function () {
         var tinggiHeader = $('#header').outerHeight();
@@ -144,8 +147,5 @@ session_start();
         });
     });
     </script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="js/main.js"></script>
 </body>
 </html>
--- periksa.php
+++ periksa.php
@@ -244,6 +244,9 @@ if (isset($_GET['aksi'])){
         ?>
     </div>
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
     <script>
     $(document).ready(function () {
         var tinggiHeader = $('#header').outerHeight();
@@ -268,8 +271,5 @@ if (isset($_GET['aksi'])){
         });
     });
     </script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="js/main.js"></script>
 </body>
 </html>
